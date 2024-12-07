ac3dd582cdb4e8455d866694067adafc6cbdee18
Sartaj Bhuvaji <s.bhuvaj@gmail.com>
2024-12-07 10:35:27 -0800

    day 7

---
+++ 14.py
@@ -0,0 +1,37 @@
+output = 0 
+
+def calculate(ans, numbers):
+    if len(numbers) == 1:
+        if numbers[0] == ans:
+            return True
+        return False
+
+    if numbers[0] > ans:
+        return False
+    
+    # multiple numbers[0], numbers[1]
+    mul = numbers[0] * numbers[1]
+    new_array1 = [mul] + numbers[2:]
+
+    # add numbers[0], numbers[1]
+    add = numbers[0] + numbers[1]
+    new_array2 = [add] + numbers[2:]
+    
+    # concatenation of numbers[0], numbers[1]
+    conc = int(str(numbers[0]) + str(numbers[1]))
+    new_array3 = [conc] + numbers[2:]
+
+    return calculate(ans, new_array1) or calculate(ans, new_array2) or calculate(ans, new_array3)
+
+
+with open('input.txt') as f:
+    for line in f:
+        ans, inp = line.split(':')
+        ans = int(ans)
+        numbers = [int(no) for no in inp.split(' ')[1:]]
+        #print(ans, numbers)
+        #print(numbers[2:])
+
+        if calculate(ans, numbers): output+=ans
+
+print(output)
